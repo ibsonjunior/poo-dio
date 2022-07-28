82f872d7ebf4a087ad46d78181aaf407055d04d3
Hugo <ibsonnjunior@gmail.com>
2022-07-28 11:13:14 -0300

    att

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import desafio.dominio.Curso;
-import desafio.dominio.Mentoria;
+import desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        //new é a maneira de instanciar a classe
+        //new é a maneira de instanciar
         Curso cursoj = new Curso();
 
         cursoj.setTitulo("Java");
@@ -18,14 +18,53 @@ public class Main {
         cursos.setDescricao("Descrição js");
         cursos.setCargaHoraria(5);
 
+        //Polimorfismo
+//        Tudo o que tem conteudo tem em curso, mas nem tudo o que tem em curso tem em conteudo
+//        Conteudo conteudo = new Curso();
+//        List<String> = new ArrayList<>();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(cursoj);
-        System.out.println(cursos);
-        System.out.println(mentoria);
+//        System.out.println(cursoj);
+//        System.out.println(cursos);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(cursoj);
+        bootcamp.getConteudos().add(cursos);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAila = new Dev();
+        devAila.setNome("Aila");
+        devAila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos " + devAila.getConteudosInscritos());
+        devAila.progredir();
+        devAila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos " + devAila.getConteudosInscritos());
+        System.out.println("Conteúdos inscritos " + devAila.getConteudosConcluidos());
+        System.out.println("XP" + devAila.calcularTotalXp());
+
+        System.out.println("-----------------");
+
+        Dev devJo = new Dev();
+        devJo.setNome("Jó");
+        devJo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos " + devJo.getConteudosInscritos());
+        devJo.progredir();
+        devJo.progredir();
+        devJo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos " + devJo.getConteudosInscritos());
+        System.out.println("Conteúdos inscritos " + devJo.getConteudosConcluidos());
+        System.out.println("XP" +  devJo.calcularTotalXp());
+
+
 
 
     }
--- src/desafio/dominio/Curso.java
+++ src/desafio/dominio/Curso.java
@@ -1,29 +1,17 @@
 package desafio.dominio;
 
-public class Curso{
+//Extends é a forma da gente trabalhar com herança
+public class Curso extends Conteudo{
     //Modificadores de acesso, encapsulamento(Public é o valor default)
 
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -37,9 +25,11 @@ public class Curso{
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
+
+
 }
